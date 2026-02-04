d75d053bd3ca73962a9138a1e736eef7b19a674c
skjalosc <oscarskjal2@gmail.com>
2026-02-04 10:37:57 +0200

    Laga till en simpel "status" endpoint för health check

--- app/main.py
+++ app/main.py
@@ -10,3 +10,13 @@ def read_root():
 @app.get("/items/{id}")
 def read_item(item_id: int, q: str = None):
     return {"id": id, "q": q}
+
+
+@app.get("/status")
+def get_status():
+    return {
+        "status": "alive",
+        "service": "cart-service",
+        "version": "0.1"
+    }
+
